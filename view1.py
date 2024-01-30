3710a88a08441fd183e78c7749712b12bcdf0fbb
azd-ui <2156798632@qq.com>
2024-01-30 16:43:57 +0800

    对author-submission图进行修改

--- view1.py
+++ view1.py
@@ -10,10 +10,13 @@ author_counts = df['author'].value_counts()
 # 筛选出提交次数超过三次的作者
 top_authors = author_counts[author_counts > 3]
 
-# 绘制柱状图
+# 绘制柱状图并调整 x 轴间距和字体大小
 plt.bar(top_authors.index, top_authors.values)
 plt.xlabel('Author')
 plt.ylabel('Submission Count')
 plt.title('Top Authors by Submission Count')
-plt.xticks(fontsize=10, rotation=45)  # 设置字体大小和旋转角度，以增大间距
+
+# 调整 x 轴刻度的位置，使间距再增大1.5倍，同时字体变小1倍
+plt.xticks(ticks=range(len(top_authors)), labels=top_authors.index, fontsize=8/1.5, rotation=60)
+
 plt.show()
